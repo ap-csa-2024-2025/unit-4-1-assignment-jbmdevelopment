41a3ab1b898a6d948803299c9d9d4c6892a7092b
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-04 11:18:02 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,30 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter any integer. (Enter -1 to stop)");
+    int num = 0;
+    int sum = 0;
+    while(!(num == -1)) {
+        num = input.nextInt();
+        sum += num;
+    }
+    sum++;
+    System.out.println("Sum is " + sum);
+    int count = 0;
+    int max = Integer.MIN_VALUE;
+    int i = 0;
+    int score = 0;
+    System.out.println("How many times do you want to input a score?");
+    count = input.nextInt();
+    while(i < count) {
+        score = input.nextInt();
+        if(score > max) {
+          max = score;
+        }
+        i++;
+    }
+    System.out.println("The largest score is " + max);
     // write solutions to problems here
   }
 }
